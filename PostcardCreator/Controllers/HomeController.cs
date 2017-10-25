18a39d17ed996c5f3fb0dc9bd4404ee4926a6e74
alorenzo <ariellorenzo08@gmail.com>
2017-10-25 12:10:28 -0400

    File upload finally working Home page UI changed and functioning well on Microsoft Edge and Google Chrome

--- PostcardCreator/Controllers/HomeController.cs
+++ PostcardCreator/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace PostcardCreator.Controllers {
     public class HomeController : Controller {
+        /// <summary>
+        /// Home page...
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index() {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload() {
+            string directory = "~/App_Data/Uploads";
+            var upload = Request.Files["file"];
+            if(upload != null && upload.ContentLength > 0) {
+                var fileName = Path.GetFileName(upload.FileName);
+                upload.SaveAs(Server.MapPath(directory + "/" + fileName));
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
